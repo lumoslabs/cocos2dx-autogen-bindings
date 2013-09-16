bba19e94fc4c69b788d7311bea23377fadc6e426
CocosRobot <redmine@cocos2d-x.org>
2013-09-16 16:14:54 +0000

    [AUTO] : autogenerated bindings

--- lua-bindings/lua_cocos2dx_auto.cpp
+++ lua-bindings/lua_cocos2dx_auto.cpp
@@ -69138,48 +69138,6 @@ int lua_cocos2dx_Menu_setOpacityModifyRGB(lua_State* tolua_S)
 #endif
     return 0;
 }
-int lua_cocos2dx_Menu_setHandlerPriority(lua_State* tolua_S)
-{
-    int argc = 0;
-    cocos2d::Menu* cobj = nullptr;
-    bool ok  = true;
-
-#if COCOS2D_DEBUG >= 1
-    tolua_Error tolua_err;
-#endif
-
-#if COCOS2D_DEBUG >= 1
-    if (!tolua_isusertype(tolua_S,1,"Menu",0,&tolua_err)) goto tolua_lerror;
-#endif
-
-    cobj = (cocos2d::Menu*)tolua_tousertype(tolua_S,1,0);
-
-#if COCOS2D_DEBUG >= 1
-    if (!cobj) 
-    {
-        tolua_error(tolua_S,"invalid 'cobj' in function 'lua_cocos2dx_Menu_setHandlerPriority'", NULL);
-        return 0;
-    }
-#endif
-
-    argc = lua_gettop(tolua_S)-1;
-    if (argc == 1) 
-    {
-        int arg0;
-        ok &= luaval_to_int32(tolua_S, 2,(int *)&arg0);
-        if(!ok)
-            return 0;
-        cobj->setHandlerPriority(arg0);
-        return 0;
-    }
-    CCLOG("%s has wrong number of arguments: %d, was expecting %d \n", "setHandlerPriority",argc, 1);
-    return 0;
-#if COCOS2D_DEBUG >= 1
-    tolua_lerror:
-    tolua_error(tolua_S,"#ferror in function 'lua_cocos2dx_Menu_setHandlerPriority'.",&tolua_err);
-#endif
-    return 0;
-}
 int lua_cocos2dx_Menu_init(lua_State* tolua_S)
 {
     int argc = 0;
@@ -69625,7 +69583,6 @@ int lua_register_cocos2dx_Menu(lua_State* tolua_S)
         tolua_function(tolua_S,"isOpacityModifyRGB",lua_cocos2dx_Menu_isOpacityModifyRGB);
         tolua_function(tolua_S,"isEnabled",lua_cocos2dx_Menu_isEnabled);
         tolua_function(tolua_S,"setOpacityModifyRGB",lua_cocos2dx_Menu_setOpacityModifyRGB);
-        tolua_function(tolua_S,"setHandlerPriority",lua_cocos2dx_Menu_setHandlerPriority);
         tolua_function(tolua_S,"init",lua_cocos2dx_Menu_init);
         tolua_function(tolua_S,"alignItemsHorizontallyWithPadding",lua_cocos2dx_Menu_alignItemsHorizontallyWithPadding);
         tolua_function(tolua_S,"alignItemsHorizontally",lua_cocos2dx_Menu_alignItemsHorizontally);
